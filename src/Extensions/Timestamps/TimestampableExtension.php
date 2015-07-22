283137f4a2319c136b3f7eebeb59c2bf7b233ae7
Michael Bender <michaelbenbender@gmail.com>
2015-07-22 10:28:01 -0400

    Change Gedmo registering to service provider

--- src/Extensions/Timestamps/TimestampableExtension.php
+++ src/Extensions/Timestamps/TimestampableExtension.php
@@ -9,20 +9,20 @@ use Doctrine\ORM\EntityManagerInterface;
 use Gedmo\Timestampable\TimestampableListener;
 use LaravelDoctrine\ORM\Extensions\GedmoExtension;
 
-class TimestampableExtension extends GedmoExtension
+class TimestampableExtension implements Extension
 {
+    public function addSubscribers(EventManager $manager, EntityManagerInterface $em, Reader $reader = null)
+    {
+        $subscriber = new TimestampableListener;
+        $subscriber->setAnnotationReader($reader);
+        $manager->addEventSubscriber($subscriber);
+    }
+
     /**
      * @return array
      */
     public function getFilters()
     {
         return [];
-    }
-
-    protected function execute(EventManager $manager, EntityManagerInterface $em, Reader $reader = null)
-    {
-        $subscriber = new TimestampableListener;
-        $subscriber->setAnnotationReader($reader);
-        $manager->addEventSubscriber($subscriber);
     }
 }
---
+++ src/GedmoExtensionServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace LaravelDoctrine\ORM;
+
+use Gedmo\DoctrineExtensions;
+use Illuminate\Support\ServiceProvider;
+use LaravelDoctrine\ORM\Extensions\DriverChain;
+
+/**
+ * Class GedmoExtensionsServiceProvider
+ * @package LaravelDoctrine\ORM
+ */
+class GedmoExtensionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the chain and reader with Gedmo
+     */
+    public function boot()
+    {
+        $driverChain = $this->app[DriverChain::class];
+
+        DoctrineExtensions::registerMappingIntoDriverChainORM(
+            $driverChain->getChain(),
+            $driverChain->getReader()
+        );
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
