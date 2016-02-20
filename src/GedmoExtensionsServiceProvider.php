06bee143707a7c045cad606b691f9733a88ed7d4
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-02-20 19:30:40 +0100

    Move gedmo stuff into register instead of boot, fixes #22

--- src/GedmoExtensionsServiceProvider.php
+++ src/GedmoExtensionsServiceProvider.php
@@ -5,14 +5,20 @@ namespace LaravelDoctrine\Extensions;
 use Doctrine\Common\Annotations\Reader;
 use Gedmo\DoctrineExtensions;
 use Illuminate\Support\ServiceProvider;
+use LogicException;
 
 class GedmoExtensionsServiceProvider extends ServiceProvider
 {
     /**
-     * Boot the service provider
+     * Register the service provider.
+     * @return void
      */
-    public function boot()
+    public function register()
     {
+        if (!$this->app->bound('registry')) {
+            throw new LogicException('[GedmoExtensionServiceProvider] requires [LaravelDoctrine\ORM\DoctrineServiceProvider] to be registered first');
+        }
+
         $registry = $this->app->make('registry');
 
         foreach ($registry->getManagers() as $manager) {
@@ -33,13 +39,5 @@ class GedmoExtensionsServiceProvider extends ServiceProvider
                 }
             }
         }
-    }
-
-    /**
-     * Register the service provider.
-     * @return void
-     */
-    public function register()
-    {
     }
 }
