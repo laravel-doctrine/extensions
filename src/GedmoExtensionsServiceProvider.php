0c8220affcbbf1f37aec6c3cdba309738b3145ee
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-02-23 18:07:59 +0100

    Listen to booting event of extension manager

--- src/GedmoExtensionsServiceProvider.php
+++ src/GedmoExtensionsServiceProvider.php
@@ -9,37 +9,32 @@ use Illuminate\Support\ServiceProvider;
 class GedmoExtensionsServiceProvider extends ServiceProvider
 {
     /**
-     * Boot the service provider
-     */
-    public function boot()
-    {
-        $registry = $this->app->make('registry');
-
-        foreach ($registry->getManagers() as $manager) {
-            $chain  = $manager->getConfiguration()->getMetadataDriverImpl();
-            $reader = $chain->getReader();
-
-            if ($reader instanceof Reader) {
-                if ($this->app->make('config')->get('doctrine.gedmo.all_mappings', false)) {
-                    DoctrineExtensions::registerMappingIntoDriverChainORM(
-                        $chain,
-                        $reader
-                    );
-                } else {
-                    DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
-                        $chain,
-                        $reader
-                    );
-                }
-            }
-        }
-    }
-
-    /**
      * Register the service provider.
      * @return void
      */
     public function register()
     {
+        $this->app['events']->listen('doctrine.extensions.booting', function () {
+            $registry = $this->app->make('registry');
+
+            foreach ($registry->getManagers() as $manager) {
+                $chain  = $manager->getConfiguration()->getMetadataDriverImpl();
+                $reader = $chain->getReader();
+
+                if ($reader instanceof Reader) {
+                    if ($this->app->make('config')->get('doctrine.gedmo.all_mappings', false)) {
+                        DoctrineExtensions::registerMappingIntoDriverChainORM(
+                            $chain,
+                            $reader
+                        );
+                    } else {
+                        DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
+                            $chain,
+                            $reader
+                        );
+                    }
+                }
+            }
+        });
     }
 }
