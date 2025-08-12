ecf52e808eff1fc2a34aa859a36cfd71efe75b85
Mason Eyre <mason.eyre@icloud.com>
2025-08-12 17:06:53 -0600

    Update GedmoExtensionsServiceProvider

--- src/GedmoExtensionsServiceProvider.php
+++ src/GedmoExtensionsServiceProvider.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace LaravelDoctrine\Extensions;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
 use Gedmo\DoctrineExtensions;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
+use LaravelDoctrine\Fluent\Extensions\GedmoExtensions;
+use LaravelDoctrine\Fluent\FluentDriver;
+
+use function method_exists;
 
 class GedmoExtensionsServiceProvider extends ServiceProvider
 {
@@ -20,16 +28,69 @@ class GedmoExtensionsServiceProvider extends ServiceProvider
             foreach ($registry->getManagers() as $manager) {
                 $chain = $manager->getConfiguration()->getMetadataDriverImpl();
 
-                if ($this->needsAllMappings()) {
-                    DoctrineExtensions::registerMappingIntoDriverChainORM($chain);
-                } else {
-                    DoctrineExtensions::registerAbstractMappingIntoDriverChainORM($chain);
+                if ($this->hasAnnotationReader($chain)) {
+                    $this->registerGedmoForAnnotations($chain);
                 }
+
+                if (! $this->hasFluentDriver($chain)) {
+                    continue;
+                }
+
+                $this->registerGedmoForFluent($chain);
             }
         });
     }
 
-    private function needsAllMappings(): mixed
+    private function hasAnnotationReader(MappingDriverChain $driver): bool
+    {
+        foreach ($driver->getDrivers() as $driver) {
+            if ($driver instanceof AnnotationDriver) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private function hasFluentDriver(MappingDriverChain $driver): bool
+    {
+        foreach ($driver->getDrivers() as $driver) {
+            if ($driver instanceof FluentDriver) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /** @throws BindingResolutionException */
+    private function registerGedmoForAnnotations(MappingDriverChain $chain): void
+    {
+        if ($this->needsAllMappings()) {
+            DoctrineExtensions::registerMappingIntoDriverChainORM($chain);
+        } else {
+            DoctrineExtensions::registerAbstractMappingIntoDriverChainORM($chain);
+        }
+
+        if (! method_exists(AnnotationRegistry::class, 'registerUniqueLoader')) {
+            return;
+        }
+
+        AnnotationRegistry::registerUniqueLoader('class_exists');
+    }
+
+    /** @throws BindingResolutionException */
+    private function registerGedmoForFluent(MappingDriverChain $chain): void
+    {
+        if ($this->needsAllMappings()) {
+            GedmoExtensions::registerAll($chain);
+        } else {
+            GedmoExtensions::registerAbstract($chain);
+        }
+    }
+
+    /** @throws BindingResolutionException */
+    private function needsAllMappings(): bool
     {
         return $this->app->make('config')->get('doctrine.gedmo.all_mappings', false) === true;
     }
